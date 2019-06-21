ee05411a0c7408033db8c5eb643d370d7eb0de97
rafamttz <ra.fa.mt@hotmail.com>
2019-06-20 22:01:02 -0300

    uiDate directive: date formater field

---
+++ js/directives/uiDate.directive.js
@@ -0,0 +1,24 @@
+'use strict'
+angular.module("listaTelefonica").directive("uiDate", function() {
+    return {
+        require: "ngModel",
+        link: function(scope, element, attrs, ctrl) {
+            var _formatDate = function(date) {
+                date = date.replace(/[^0-9]+/g, "");
+                if(date.length > 2) {
+                    date = date.substring(0, 2) + "/" + date.substring(2);
+                }
+                if(date.length > 5) {
+                    date = date.substring(0, 5) + "/" + date.substring(5, 9);
+                }
+
+                return date;
+            }
+
+            element.bind("keyup", () => {
+                ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+                ctrl.$render();
+            });
+        }
+    };
+});
